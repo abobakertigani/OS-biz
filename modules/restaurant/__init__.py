df54ad6338d80121d304b828641503b4633a26c8
abobakertigani <56824184+abobakertigani@users.noreply.github.com>
2025-07-30 09:37:31 +0200

    Update __init__.py

--- modules/restaurant/__init__.py
+++ modules/restaurant/__init__.py
@@ -1,1 +1,12 @@
+# modules/restaurant/__init__.py
 
+from .models import register_models
+from .routes import register_routes
+
+def register_module(app, db):
+    """
+    هذه الدالة هي نقطة الدخول للوحدة
+    تُستدعى من النواة (kernel)
+    """
+    register_models(db)
+    register_routes(app, db)
