b7ec5126a9a1d7529368de6ff72a84e0024ce0d7
abobakertigani <56824184+abobakertigani@users.noreply.github.com>
2025-07-30 09:25:06 +0200

    Create auth.py

---
+++ core/auth.py
@@ -0,0 +1,13 @@
+# osbiz/core/auth.py
+
+from flask_login import UserMixin
+from .database import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(50), nullable=False)  # admin, cashier, pharmacist...
+
+    def __repr__(self):
+        return f'<User {self.username} - {self.role}>'
