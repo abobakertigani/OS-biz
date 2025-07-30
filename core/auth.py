1442057140a8919824865f4b3b121f9869cf2fbf
abobakertigani <56824184+abobakertigani@users.noreply.github.com>
2025-07-30 20:59:56 +0200

    Update auth.py

--- core/auth.py
+++ core/auth.py
@@ -1,13 +1,21 @@
-# osbiz/core/auth.py
+# core/auth.py
 
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from .database import db
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    role = db.Column(db.String(50), nullable=False)  # admin, cashier, pharmacist...
+    password_hash = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(50), nullable=False)  # admin, cashier, chef, pharmacist...
+    full_name = db.Column(db.String(100))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User {self.username} - {self.role}>'
